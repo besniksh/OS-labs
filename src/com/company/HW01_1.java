2f2245f2100547cf44fd85d8bd84ff248124cafd
Besnik Shabani <besniksh95@gmail.com>
2017-02-26 02:29:58 +0100

    Lab1. Zadaca2 done

---
+++ src/com/company/HW01_1.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class HW01_1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        //System.out.println("Hello World!");
+        if(args.length == 0){
+            System.out.println("Ne e vnesen nitu eden argument");
+            return;
+        }
+        File imenik = new File(args[0]);
+        if(!imenik.exists()){
+            System.out.println("Ne postoi imenik so takvo ime!");
+            return;
+        }
+        if(!imenik.isDirectory()){
+            System.out.println("Fajlot so takvo ime ne e imenik!");
+        }
+
+        File[] files = imenik.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith("txt");
+            }
+        });
+
+        long vkupno=0;
+
+        for(File f:files){
+            vkupno += f.length();
+        }
+
+        double prosek = vkupno/files.length;
+
+        System.out.println("Vkupnataa golemina na .txt fajlovite e "+vkupno+" bajti\n A prosocntata golemina e " +prosek+ " bajti");
+
+
+    }
+}
---
+++ src/com/company/HW01_2.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HW01_2{
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis;
+
+        try{
+            fis = new FileInputStream("data/izvor.txt");
+        } catch(FileNotFoundException e){
+            System.out.println("Ne e pronajden takov fajl!");
+            return;
+        }
+
+        ArrayList<Integer> tekst = new ArrayList<Integer>();
+        int bajt = 0;
+
+        while((bajt = fis.read()) != -1)
+            tekst.add(bajt);
+
+        fis.close();
+
+        FileOutputStream fos;
+
+        try{
+            fos = new FileOutputStream("data/destinacija.txt");
+        } catch(FileNotFoundException e){
+            System.out.println("Ne e mozno da se otvori fajlot!");
+            return;
+        }
+
+        for (int i=tekst.size()-1; i>=0; i--)
+            fos.write(tekst.get(i));
+
+        fos.flush();
+        fos.close();
+
+
+    }
+}
